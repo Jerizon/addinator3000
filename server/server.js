883fc5d8871a81a1dc92b2abcd3e03dfe7da5a5d
Jerizon <JesseLowellReyes@gmail.com>
2017-04-20 11:50:21 -0700

    Reorganized file tree structure

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer(function(request, response) {
+	response.end('Hello world!');
+
+});
+
+server.listen(3000, function(){
+	console.log('Server listening...');
+})
